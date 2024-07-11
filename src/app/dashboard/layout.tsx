458d167232ea67366e0c8bbc85a7e3d154b22b6e
Vorashil Farzaliyev <vofarzaliyev@gmail.com>
2024-07-11 16:27:03 +0100

    removed unnecessary api routes and made server component even simpler

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,16 @@
 import {ReactNode} from "react";
 
-export default function DashboardLayout({children}: { children: ReactNode }) {
+export default async function DashboardLayout({children}: { children: ReactNode }) {
+    const userRole = await getUserRole()
     return (
         <section>
             {/* Include shared UI here e.g. a header or sidebar */}
             {children}
         </section>
     )
+}
+
+type UserRole = "admin" | "user" | "finance"
+async function getUserRole(): Promise<UserRole>{
+    return Promise.resolve("user")
 }
